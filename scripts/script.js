e06491663d7afdbd32e3cdf77203a5d7dac997d0
MoisesVB <moisess.vieiraa.beltramee@gmail.com>
2022-01-25 16:47:18 -0300

    adding sound effects

--- scripts/script.js
+++ scripts/script.js
@@ -190,6 +190,8 @@ function addToggleCheckboxEventListener(element, todoObject) {
     element.addEventListener('click', () => {
         if (todoObject.done === false) {
             toggleCheckbox(todoObject, true);
+            new Audio('../assets/effect.mp3').play();
+            
         } else if (todoObject.done === true) {
             toggleCheckbox(todoObject, false);
         }
